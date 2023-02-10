bc0dd12b836aadb84276b322b922039186e6ebc1
AaryanAlam <96620363+AaryanAlam@users.noreply.github.com>
2023-02-10 18:15:03 +0100

    health bar phase 1 (WIP)

---
+++ LifelessShadows/Assets/Scripts/Health.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Health : MonoBehaviour
+{
+    public Slider fill;
+    public int health = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            health = health - 1;
+            
+        }
+    }
+}
