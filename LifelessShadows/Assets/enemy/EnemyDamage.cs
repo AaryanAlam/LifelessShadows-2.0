777ae13011b2234d6215bdf2cb023ca76c5059c1
AaryanAlam <96620363+AaryanAlam@users.noreply.github.com>
2023-02-23 20:39:34 +0100

    do not move wooden cabin¨

--- LifelessShadows/Assets/enemy/EnemyDamage.cs
+++ LifelessShadows/Assets/enemy/EnemyDamage.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour
 {
+    public int EnemyHealth = 100;
     public Health PlayerHealth;
     // Start is called before the first frame update
     void Start()
@@ -14,7 +15,10 @@ public class EnemyDamage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (EnemyHealth <= 0) 
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerStay(Collider other) {
