19ae402f0f34073d920fd162b2097906da405120
AaryanAlam <96620363+AaryanAlam@users.noreply.github.com>
2023-02-26 20:19:48 +0100

    damage vfx

--- LifelessShadows/Assets/Scripts/enemySpawner.cs
+++ LifelessShadows/Assets/Scripts/enemySpawner.cs
@@ -6,46 +6,29 @@ using UnityEngine.AI;
 public class enemySpawner : MonoBehaviour
 {
      public GameObject enemyPrefab;
-    public int initialNumberOfEnemiesToSpawn = 10;
+    public int numberOfEnemiesToSpawn = 10;
     public float spawnRadius = 10f;
     public Terrain terrain;
-    public float timeBetweenSpawns = 60f;
-
-    private int numberOfEnemiesToSpawn;
-    private float timeSinceLastSpawn;
 
     private void Start()
     {
-        numberOfEnemiesToSpawn = initialNumberOfEnemiesToSpawn;
-        timeSinceLastSpawn = 0f;
-    }
-
-    private void Update()
-    {
-        timeSinceLastSpawn += Time.deltaTime;
-        if (timeSinceLastSpawn >= timeBetweenSpawns)
-        {
-            numberOfEnemiesToSpawn++;
-            timeSinceLastSpawn = 0f;
-        }
-
-        SpawnEnemies();
-    }
-
-    private void SpawnEnemies()
-    {
         for (int i = 0; i < numberOfEnemiesToSpawn; i++)
         {
+            // Calculate a random position within the spawn radius.
             Vector3 randomPosition = transform.position + Random.insideUnitSphere * spawnRadius;
+            // Set the Y coordinate of the random position to the height of the terrain at that location.
             randomPosition.y = terrain.SampleHeight(randomPosition);
 
+            // If the random position is outside of the terrain bounds, try again with a new position.
             while (!IsPositionOnTerrain(randomPosition))
             {
                 randomPosition = transform.position + Random.insideUnitSphere * spawnRadius;
                 randomPosition.y = terrain.SampleHeight(randomPosition);
             }
 
+            // Instantiate the enemy prefab at the random position.
             GameObject enemy = Instantiate(enemyPrefab, randomPosition, Quaternion.identity);
+            // Set the destination of the navmesh agent to a random point on the terrain.
             NavMeshAgent navMeshAgent = enemy.GetComponentInChildren<NavMeshAgent>();
             navMeshAgent.SetDestination(GetRandomPointOnTerrain());
         }
@@ -53,15 +36,19 @@ public class enemySpawner : MonoBehaviour
 
     private bool IsPositionOnTerrain(Vector3 position)
     {
+        // Get the position of the terrain object in world space.
         Vector3 terrainPosition = terrain.transform.position;
+        // Get the size of the terrain in world space.
         Vector3 terrainSize = terrain.terrainData.size;
 
+        // Check if the position is inside the bounds of the terrain.
         if (position.x < terrainPosition.x || position.x > terrainPosition.x + terrainSize.x ||
             position.z < terrainPosition.z || position.z > terrainPosition.z + terrainSize.z)
         {
             return false;
         }
 
+        // Check if the position is above the terrain surface.
         float terrainHeight = terrain.SampleHeight(position);
         if (position.y < terrainHeight)
         {
@@ -73,9 +60,12 @@ public class enemySpawner : MonoBehaviour
 
     private Vector3 GetRandomPointOnTerrain()
     {
+        // Generate a random point on the terrain by selecting random X and Z coordinates within the terrain bounds.
         float randomX = Random.Range(0f, terrain.terrainData.size.x);
         float randomZ = Random.Range(0f, terrain.terrainData.size.z);
+        // Get the height of the terrain at the selected X and Z coordinates.
         float terrainHeight = terrain.SampleHeight(new Vector3(randomX, 0f, randomZ));
+        // Return the random point on the terrain.
         return new Vector3(randomX, terrainHeight, randomZ);
     }
 }
--- LifelessShadows/Assets/enemy/EnemyDamage.cs
+++ LifelessShadows/Assets/enemy/EnemyDamage.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class EnemyDamage : MonoBehaviour
 {
     public int EnemyHealth = 100;
     private Health PlayerHealth;
 
+    private GameObject damVFX;
+
+    private Image damVFXimg;
+
     private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
+        damVFX = GameObject.FindWithTag("damage");
+        damVFXimg = damVFX.GetComponent<Image>();
         player = GameObject.FindWithTag("Player");
         PlayerHealth = player.GetComponent<Health>();
     }
@@ -27,10 +35,23 @@ public class EnemyDamage : MonoBehaviour
     private void OnTriggerStay(Collider other) {
         if (other.CompareTag("Player")) {
             Damage(1);
+            effectOn();
         }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        Invoke("effectOff", 2f);
     }
     public void Damage(int damage) 
     {
         PlayerHealth.health -= damage;
+    }
+
+    public void effectOn() {
+        damVFXimg.enabled = true;
+    }
+
+    public void effectOff() {
+        damVFXimg.enabled = false;
     }
 }
