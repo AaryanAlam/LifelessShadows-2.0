9121cb91e6c566f0352c139d7ccba1bfd9a47692
AaryanAlam <96620363+AaryanAlam@users.noreply.github.com>
2023-03-29 21:01:39 +0200

    oop

--- LifelessShadows/Assets/Scripts/logbookSTART.cs
+++ LifelessShadows/Assets/Scripts/logbookSTART.cs
@@ -4,16 +4,64 @@ using UnityEngine;
 using UnityEngine.UI;
 public class logbookSTART : MonoBehaviour
 {
-    public
+    public Canvas canvas;
+    public int hasPickedUpBook;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!PlayerPrefs.HasKey("hasPickedUpBook"))
+        {
+            PlayerPrefs.SetInt("hasPickedUpBook", 0);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (hasPickedUpBook == 1) {
+            Destroy(gameObject);
+                }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            canvas.enabled = true;
+            if (Input.GetKeyDown(KeyCode.T)) {
+                hasPickedUpBook = 1;
+                Destroy(gameObject);
+                Save(); 
+            }
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (hasPickedUpBook == 1  && other.CompareTag("Player"))
+        {
+            canvas.enabled = false;
+        }
+    }
+
+    public void Save()
+    {
+        if (hasPickedUpBook == 1)
+        {
+            PlayerPrefs.SetInt("hasPickedUpBook", 1);
+        }
+        else if ( hasPickedUpBook == 0)
+        {
+            PlayerPrefs.SetInt("hasPickedUpBook", 0);
+        }
+    }
+    public void Load()
+    {
+        hasPickedUpBook = PlayerPrefs.GetInt("hasPickedUpBook");
     }
 }
